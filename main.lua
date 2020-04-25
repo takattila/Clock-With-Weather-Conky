44124fd0359f9e77e2f72b27b0a62c93f9d2c6e4
takattila <takattila@gmail.com>
2020-04-25 20:22:25 +0200

    Set local variables

--- main.lua
+++ main.lua
@@ -36,10 +36,10 @@ function image(cr, pos_x, pos_y, transparency, image_name)
 end
 
 function text(cr, pos_x, pos_y, transparency, text, font_face, font_size, font_weight)
-	r_text, g_text, b_text = hex2rgb(settings.appearance.html_text_color)
+	local r_text, g_text, b_text = hex2rgb(settings.appearance.html_text_color)
 	cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE)
 	cairo_set_source_rgba(cr, r_text, g_text, b_text, transparency)
-	ct = cairo_text_extents_t:create()
+	local ct = cairo_text_extents_t:create()
 	cairo_select_font_face(cr, font_face, CAIRO_FONT_SLANT_NORMAL, font_weight)
 	cairo_set_font_size(cr, font_size)
 	cairo_text_extents(cr,text,ct)
@@ -50,45 +50,45 @@ end
 
 function draw_elements(cr, weather_json)
 	if weather_json ~= "" then
-		obj = json.decode(weather_json)
+		local obj = json.decode(weather_json)
 
 		------------------------------------------------------------------------------------------
 		-- CLOCK section
 		------------------------------------------------------------------------------------------
 
 		-- Date / year
-		year = conky_parse("${exec date '+%Y.'}")
+		local year = conky_parse("${exec date '+%Y.'}")
 		text(cr, 20, 30, settings.appearance.transparency_full, year, settings.appearance.default_font_face, 20, CAIRO_FONT_WEIGHT_NORMAL)
 
 		------------------------------------------------------------------------------------------
 
 		-- Date / month text + day number
-		date = conky_parse("${exec date '+| %B %d. | %A'}")
+		local date = conky_parse("${exec date '+| %B %d. | %A'}")
 		--date = "| Wednesday | December 31."
 		text(cr, 70, 30, settings.appearance.transparency_half, date, settings.appearance.default_font_face, 20, CAIRO_FONT_WEIGHT_NORMAL)
 
 		------------------------------------------------------------------------------------------
 
 		-- Hour
-		hour = conky_parse("${exec date '+%H'}")
+		local hour = conky_parse("${exec date '+%H'}")
 		text(cr, 10, 155, settings.appearance.transparency_full, hour, settings.appearance.default_font_face, 145, CAIRO_FONT_WEIGHT_NORMAL)
 
 		------------------------------------------------------------------------------------------
 
 		-- Minutes
-		minutes = conky_parse("${exec date '+:%M'}")
+		local minutes = conky_parse("${exec date '+:%M'}")
 		text(cr, 170, 155, settings.appearance.transparency_half, minutes, settings.appearance.default_font_face, 145, CAIRO_FONT_WEIGHT_NORMAL)
 
 		------------------------------------------------------------------------------------------
 
 		-- Seconds
-		seconds = ": " .. conky_parse("${exec date '+%S'}")
+		local seconds = ": " .. conky_parse("${exec date '+%S'}")
 		text(cr, 370, 155, settings.appearance.transparency_full, seconds, settings.appearance.default_font_face, 20, CAIRO_FONT_WEIGHT_NORMAL)
 
 		------------------------------------------------------------------------------------------
 
 		-- HDD
-		hdd = "HDD"
+		local hdd = "HDD"
 		text(cr, 20, 180, settings.appearance.transparency_full, hdd, settings.appearance.default_font_face, 15, CAIRO_FONT_WEIGHT_NORMAL) 
 
 		hdd = conky_parse("${fs_free} / ${fs_size}")
@@ -97,7 +97,7 @@ function draw_elements(cr, weather_json)
 		------------------------------------------------------------------------------------------
 
 		-- RAM
-		ram = "RAM"
+		local ram = "RAM"
 		text(cr, 200, 180, settings.appearance.transparency_full, ram, settings.appearance.default_font_face, 15, CAIRO_FONT_WEIGHT_NORMAL) 
 
 		ram = conky_parse("${mem} / ${memmax}")
@@ -106,7 +106,7 @@ function draw_elements(cr, weather_json)
 		------------------------------------------------------------------------------------------
 
 		-- CPU
-		cpu = "CPU"
+		local cpu = "CPU"
 		text(cr, 20, 200, settings.appearance.transparency_full, cpu, settings.appearance.default_font_face, 15, CAIRO_FONT_WEIGHT_NORMAL) 
 
 		cpu = conky_parse("${cpu cpu0}%")
@@ -115,7 +115,7 @@ function draw_elements(cr, weather_json)
 		------------------------------------------------------------------------------------------
 
 		-- SWAP
-		swap = "SWAP"
+		local swap = "SWAP"
 		text(cr, 200, 200, settings.appearance.transparency_full, swap, settings.appearance.default_font_face, 15, CAIRO_FONT_WEIGHT_NORMAL) 
 
 		swap = conky_parse("${swapperc}% (size: ${swapmax})")
@@ -131,7 +131,7 @@ function draw_elements(cr, weather_json)
 		------------------------------------------------------------------------------------------
 
 		-- Weather icon
-		weather_icon = obj.weather[1].icon
+		local weather_icon = obj.weather[1].icon
 		image(cr, 470, 45, settings.appearance.transparency_weather_icon, weather_icon)
 
 		------------------------------------------------------------------------------------------
@@ -145,7 +145,7 @@ function draw_elements(cr, weather_json)
 		-- Temperature -> Current
 		image(cr, 440, 140, settings.appearance.transparency_half, "temperature")
 
-		temperature = obj.main.temp
+		local temperature = obj.main.temp
 		temperature = string.format("%.0f", (temperature or 0)) .. "˚C"
 
 		text(cr, 460, 155, settings.appearance.transparency_full, temperature, settings.appearance.default_font_face, 40, CAIRO_FONT_WEIGHT_BOLD) 
@@ -155,7 +155,7 @@ function draw_elements(cr, weather_json)
 		-- Temperature -> Details
 		image(cr, 435, 175, settings.appearance.transparency_half, "arrow-right")
 
-		details = obj.weather[1].description
+		local details = obj.weather[1].description
 		text(cr, 445, 180, settings.appearance.transparency_half, details, settings.appearance.default_font_face, 15, CAIRO_FONT_WEIGHT_NORMAL)
 
 		------------------------------------------------------------------------------------------
@@ -163,7 +163,7 @@ function draw_elements(cr, weather_json)
 		-- Temperature -> MIN
 		image(cr, 435, 195, settings.appearance.transparency_half, "arrow-down")
 
-		temp_min = obj.main.temp_min
+		local temp_min = obj.main.temp_min
 		temp_min = string.format("%.0f", (temp_min or 0)) .. "˚C"
 
 		text(cr, 445, 200, settings.appearance.transparency_full, temp_min, settings.appearance.default_font_face, 15, CAIRO_FONT_WEIGHT_NORMAL)
@@ -173,7 +173,7 @@ function draw_elements(cr, weather_json)
 		-- Temperature -> MAX
 		image(cr, 495, 195, settings.appearance.transparency_half, "arrow-up")
 
-		temp_max = obj.main.temp_max
+		local temp_max = obj.main.temp_max
 		temp_max = string.format("%.0f", (temp_max or 0)) .. "˚C"
 
 		text(cr, 505, 200, settings.appearance.transparency_full, temp_max, settings.appearance.default_font_face, 15, CAIRO_FONT_WEIGHT_NORMAL)
@@ -183,7 +183,7 @@ function draw_elements(cr, weather_json)
 		-- Temperature -> Feels like
 		image(cr, 555, 195, settings.appearance.transparency_half, "white-man")
 
-		feels_like = obj.main.feels_like
+		local feels_like = obj.main.feels_like
 		feels_like = string.format("%.0f", (feels_like or 0)) .. "˚C"
 
 		text(cr, 565, 200, settings.appearance.transparency_full, feels_like, settings.appearance.default_font_face, 15, CAIRO_FONT_WEIGHT_NORMAL)
@@ -191,7 +191,7 @@ function draw_elements(cr, weather_json)
 end
 
 function hex2rgb(hex)
-	hex = hex:gsub("#","")
+	local hex = hex:gsub("#","")
 	return (tonumber("0x"..hex:sub(1,2))/255), 
 		(tonumber("0x"..hex:sub(3,4))/255), 
 		tonumber(("0x"..hex:sub(5,6))/255)
@@ -199,22 +199,22 @@ end
 
 function is_set_api_key(cr)
 	if settings.weather.api_key == nil then
-		error_text_1 = "ERROR :("
+		local error_text_1 = "ERROR :("
 		text(cr, 0, 40, settings.appearance.transparency_full, error_text_1, settings.appearance.default_font_face, 40, CAIRO_FONT_WEIGHT_BOLD)
 		
-		error_text_2 ="The 'OPENWEATHER_API_KEY' environment variable must be exported!"
+		local error_text_2 ="The 'OPENWEATHER_API_KEY' environment variable must be exported!"
 		text(cr, 0, 70, settings.appearance.transparency_full, error_text_2, settings.appearance.default_font_face, 20, CAIRO_FONT_WEIGHT_normal)
 		
-		error_text_3 ="   1. Sign up on http://openweathermap.org to get an API key."
+		local error_text_3 ="   1. Sign up on http://openweathermap.org to get an API key."
 		text(cr, 0, 90, settings.appearance.transparency_full, error_text_3, settings.appearance.default_font_face, 15, CAIRO_FONT_WEIGHT_NORMAL)
 		
-		error_text_4 ="   2. After the registration Check your e-mail, the API key should be sent!"
+		local error_text_4 ="   2. After the registration Check your e-mail, the API key should be sent!"
 		text(cr, 0, 110, settings.appearance.transparency_full, error_text_4, settings.appearance.default_font_face, 15, CAIRO_FONT_WEIGHT_NORMAL)
 		
-		error_text_5 ="   3. Open a terminal and export the API key:"
+		local error_text_5 ="   3. Open a terminal and export the API key:"
 		text(cr, 0, 130, settings.appearance.transparency_full, error_text_5, settings.appearance.default_font_face, 15, CAIRO_FONT_WEIGHT_NORMAL)
 		
-		error_text_6 ="       export OPENWEATHER_API_KEY=<YOUR-API-KEY>"
+		local error_text_6 ="       export OPENWEATHER_API_KEY=<YOUR-API-KEY>"
 		text(cr, 0, 150, settings.appearance.transparency_full, error_text_6, settings.appearance.default_font_face, 15, CAIRO_FONT_WEIGHT_NORMAL)
 
 		print(
